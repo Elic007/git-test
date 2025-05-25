41d1b071ad81bff2da6d6b42e19db5e428adf9dc
xhc <x2168476168@163.com>
2025-05-25 16:58:15 +0800

    hot-fix-1 commit

--- src/main/java/com/example/hello.java
+++ src/main/java/com/example/hello.java
@@ -5,5 +5,6 @@ public class hello {
         System.out.println("hello");
         System.out.println("hello2.0");
         System.out.println("hello3.0");
+        System.out.println("hello4.0");
     }
 }
